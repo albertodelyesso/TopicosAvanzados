815ab29fbe0da3c34a3c5ad5ede1198f570a8a32
Albeto A. Del Yesso <aa_dy@hotmail.com>
2017-08-17 11:49:56 -0300

    Primer commit

---
+++ src/main.java
@@ -0,0 +1,24 @@
+import javax.lang.model.element.UnknownAnnotationValueException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alberto del yesso
+ */
+
+
+public class main {
+    
+public static void main(String[] args) {
+    ventanaMenu unaVentanaMenu = new ventanaMenu();
+    unaVentanaMenu.setVisible(true);
+}    
+    
+     
+        
+}
